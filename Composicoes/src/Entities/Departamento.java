2f66540f4425e6ca9630fcafc0357f31f62c4c26
Thiago Prado <thiago_cpv92@hotmail.com>
2022-07-27 18:35:38 -0300

    Estrutura inicial

---
+++ Composicoes/src/Entities/Departamento.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public class Departamento {
+	
+	private String name;
+	
+	public Departamento() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
